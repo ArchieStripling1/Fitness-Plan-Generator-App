2360f677d4bb87b878e174522a364aef70067ffc
ArchieStripling1 <Archiestripling7@gmail.com>
2026-09-05 18:51:56 +0100

    E203 whitespace before ':' error
    Signed-off-by: ArchieStripling1 <Archiestripling7@gmail.com>

--- Dev/Data/LongRunData.py
+++ Dev/Data/LongRunData.py
@@ -2,14 +2,14 @@ class LongRunData:
 
     # Long runs types for novice level
     novice_long_run_types = {
-        "easy_+_steady_finish" : {
-            "warmup_portion" : 0.8,
-            "quality_portion" : 0.2,
+        "easy_+_steady_finish": {
+            "warmup_portion": 0.8,
+            "quality_portion": 0.2,
         },
-        "easy_+_10k_finish" : {
+        "easy_+_10k_finish": {
 
         },
-        "progression_long_run" : {
+        "progression_long_run": {
 
         },
         "Practice Race long run": {
@@ -19,18 +19,18 @@ class LongRunData:
 
     # Long runs types for intermediate level
     intermediate_long_run_types = {
-        "easy_+_steady" : {
-            "warmup_portion" : 0.5,
-            "quality_portion" : 0.5,
+        "easy_+_steady": {
+            "warmup_portion": 0.5,
+            "quality_portion": 0.5,
         },
-        "easy_+_tempo" : {
-            "warmup_portion" : 0.6,
-            "quality_portion" : 0.4,
+        "easy_+_tempo": {
+            "warmup_portion": 0.6,
+            "quality_portion": 0.4,
         },
-        "easy_+_race_pace" : {
+        "easy_+_race_pace": {
 
         },
-        "progression" : {
+        "progression": {
 
         },
         "Practice Race long run": {
@@ -40,24 +40,24 @@ class LongRunData:
 
     # Long runs types for Advanced level
     advanced_long_run_types = {
-        "easy_+_marathon_pace_+_easy" : {
-            "warmup_portion" : 0.4,
-            "quality_portion" : 0.4,
-            "cooldown_portion" : 0.2,
-            "pace" : "marathon"
+        "easy_+_marathon_pace_+_easy": {
+            "warmup_portion": 0.4,
+            "quality_portion": 0.4,
+            "cooldown_portion": 0.2,
+            "pace": "marathon"
         },
-        "easy_+_threshold_+_easy" : {
-            "warmup_portion" : 0.4,
-            "quality_portion" : 0.2,
-            "cooldown_portion" : 0.2,
+        "easy_+_threshold_+_easy": {
+            "warmup_portion": 0.4,
+            "quality_portion": 0.2,
+            "cooldown_portion": 0.2,
         },
-        "fast_finish" : {
+        "fast_finish": {
 
         },
-        "alternating_pace" : {
+        "alternating_pace": {
 
         },
-        "race_pace_blocks" : {
+        "race_pace_blocks": {
 
         },
         "Practice Race long run": {
