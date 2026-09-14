4b72918c221e8c8eed76ca5db16fc73831de53b8
ArchieStripling1 <Archiestripling7@gmail.com>
2026-09-14 18:20:52 +0100

    Added basic visual to calendarScreen
    Signed-off-by: ArchieStripling1 <Archiestripling7@gmail.com>

--- Dev/UI/DefaultScreens/CalendarScreen.py
+++ Dev/UI/DefaultScreens/CalendarScreen.py
@@ -1,7 +1,10 @@
 from kivy.app import App
+from kivy.graphics import Color, RoundedRectangle
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
+from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
+from Dev.UI.Theme import TEXT, CARD
 from datetime import date, timedelta
 
 
@@ -36,6 +39,56 @@ class CalendarScreen(Screen):
         plan = data["GeneratedPlan"]
 
         print(dates, plan)
+
+        # Initialize Card
+        header_card = BoxLayout(
+            orientation='vertical',
+            spacing=15,
+            padding=20,
+            size_hint_y=None
+        )
+
+        # Dynamic height
+        header_card.bind(minimum_height=header_card.setter("height"))
+
+        # Card background
+        with header_card.canvas.before:
+            Color(*CARD)
+            header_card.rect = RoundedRectangle(
+                pos=header_card.pos,
+                size=header_card.size,
+                radius=[25]
+            )
+
+        # Keep card updated
+        def update_rect(instance, value):
+            instance.rect.pos = instance.pos
+            instance.rect.size = instance.size
+
+        # Bind Card with updated variables
+        header_card.bind(pos=update_rect, size=update_rect)
+
+        header = BoxLayout(
+            orientation="vertical",
+            size_hint_y=None,
+            height=70,
+            spacing=5
+        )
+
+        title_label = Label(
+            text="Your Calender",
+            font_size=32,
+            bold=True,
+            color=TEXT,
+            size_hint_y=None,
+            height=40
+        )
+
+        header.add_widget(title_label)
+
+        header_card.add_widget(header)
+
+        self.content.add_widget(header_card)
 
     # Function to create dates for each day of plan.
     def workout_dates(self):
